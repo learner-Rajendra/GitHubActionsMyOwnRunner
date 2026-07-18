f888145c408c622625d64ca2a638b4a94a01e376
learner-Rajendra <learn.rajendra@gmail.com>
2026-07-18 23:20:37 +0530

    Added job success and failure message

--- printo.py
+++ printo.py
@@ -1,1 +1,10 @@
+import sys
+
 print "Hello buddy .. it me !!! Thanks to Gemini mate"
+
+
+if error_condition_detected:
+    print "Error -- Automation step failed!"
+    sys.exit(1)
+
+print "Success! job executed."
